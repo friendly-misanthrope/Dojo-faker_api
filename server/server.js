c3392e9d25fa70be1b78b9dfb17de8df547455f7
NickBarkerDev <nickthedevguy@gmail.com>
2023-04-07 03:35:25 -0700

    added function to create new user object

--- server/server.js
+++ server/server.js
@@ -3,3 +3,19 @@ const app = express()
 const port=8000
 const { faker } = require("@faker-js/faker")
 
+// Middleware for POST requests
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// Function to create new user
+const createUser = () => {
+  const newUser = {
+    _id: faker.random.numeric(2),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    phoneNumber: `${faker.random.numeric(3)}-${faker.random.numeric(3)}-${faker.random.numeric(4)}`
+  }
+  return newUser
+}
